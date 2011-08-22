c680f18ae75b1ff99e8c919e7e0b94ad300bf17c
Geir Ove Grønmo <grove@geirove.org>
2011-08-22 16:30:50 +0200

    Added default constructor to FieldData.Message.

--- presto-utils/src/main/java/net/ontopia/presto/jaxb/FieldData.java
+++ presto-utils/src/main/java/net/ontopia/presto/jaxb/FieldData.java
@@ -45,6 +45,10 @@ public class FieldData {
     public static class Message {
         private String type;
         private String message;
+
+        public Message() {
+        }
+        
         public Message(String type, String message) {
             this.type = type;
             this.message = message;
