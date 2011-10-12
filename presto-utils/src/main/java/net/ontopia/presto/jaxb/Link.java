9d5d1eb91a11295d143af89a88e9f235153e5522
Geir Ove Grønmo <grove@geirove.org>
2011-10-12 15:31:30 +0200

    Added Jackson annotations to Link class.

--- presto-utils/src/main/java/net/ontopia/presto/jaxb/Link.java
+++ presto-utils/src/main/java/net/ontopia/presto/jaxb/Link.java
@@ -1,5 +1,10 @@
 package net.ontopia.presto.jaxb;
 
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+@JsonSerialize(include=JsonSerialize.Inclusion.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown=true)
 public class Link {
     
     private String name;
