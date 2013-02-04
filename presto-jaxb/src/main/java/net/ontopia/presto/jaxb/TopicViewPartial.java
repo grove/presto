acca9052ac1989b5bf1253ad6d0f7bdb2b8b68df
Geir Ove Grønmo <grove@geirove.org>
2013-02-04 19:37:33 +0100

    Added TopicViewPartial.

---
+++ presto-jaxb/src/main/java/net/ontopia/presto/jaxb/TopicViewPartial.java
@@ -0,0 +1,33 @@
+package net.ontopia.presto.jaxb;
+
+import java.util.Collection;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.map.annotate.JsonSerialize;
+
+@XmlRootElement
+@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TopicViewPartial {
+    private String format = "topic-view-partial";
+    private Collection<FieldData> fields;
+
+    public String getFormat() {
+        return format;
+    }
+    
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public Collection<FieldData> getFields() {
+        return fields;
+    }
+
+    public void setFields(Collection<FieldData> fields) {
+        this.fields = fields;
+    }
+
+}
