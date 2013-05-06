fa11247308c59445256d4438e462d53359dcdf47
Geir Ove Grønmo <grove@geirove.org>
2013-05-06 09:10:07 +0200

    Added bogus id to test to see if non-existant ids cause null return values.

--- presto-test/src/test/java/net/ontopia/presto/jaxrs/PrestoTest.java
+++ presto-test/src/test/java/net/ontopia/presto/jaxrs/PrestoTest.java
@@ -106,7 +106,7 @@ public class PrestoTest {
         
         PrestoTopic deletedJohn = dataProvider.getTopicById(foundJohn.getId());
         Assert.assertNull(deletedJohn);
-        topicsByIds = dataProvider.getTopicsByIds(Arrays.asList(createdJohn.getId(), createdDanny.getId()));
+        topicsByIds = dataProvider.getTopicsByIds(Arrays.asList(createdJohn.getId(), createdDanny.getId(), "bogus-id"));
         Assert.assertEquals(1, topicsByIds.size());
         Assert.assertTrue(topicsByIds.contains(createdDanny));
     }
