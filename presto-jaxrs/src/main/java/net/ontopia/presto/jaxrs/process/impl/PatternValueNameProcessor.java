0813ab0bc67c2c5e8bd9888ecce64dae9b66bfe3
Geir Ove Grønmo <grove@geirove.org>
2013-06-13 09:52:36 +0200

    Refactored PatternValueUtils.

--- presto-jaxrs/src/main/java/net/ontopia/presto/jaxrs/process/impl/PatternValueNameProcessor.java
+++ presto-jaxrs/src/main/java/net/ontopia/presto/jaxrs/process/impl/PatternValueNameProcessor.java
@@ -1,18 +1,10 @@
 package net.ontopia.presto.jaxrs.process.impl;
 
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import net.ontopia.presto.jaxrs.PrestoContext;
 import net.ontopia.presto.jaxrs.process.ValueProcessor;
 import net.ontopia.presto.spi.PrestoFieldUsage;
-import net.ontopia.presto.spi.PrestoSchemaProvider;
 import net.ontopia.presto.spi.PrestoTopic;
 import net.ontopia.presto.spi.utils.PrestoTopicFieldVariableResolver;
-
-import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.node.ObjectNode;
 
 public class PatternValueNameProcessor extends ValueProcessor {
 
@@ -23,44 +15,12 @@ public class PatternValueNameProcessor extends ValueProcessor {
 
     @Override
     public String getName(PrestoContext context, PrestoFieldUsage field, PrestoTopic topic) {
-        String name = topic.getName(field);
-        ObjectNode config = getConfig();
-        if (config != null) {
-            JsonNode patternNode = config.path("pattern");
-            if (patternNode.isTextual()) {
-                String pattern = patternNode.getTextValue();
-                Pattern p = Pattern.compile("\\$\\{(\\w+)\\}");
-                if (name != null) {
-                    name = replacePattern(p, pattern, name, topic);
-                }
-            }
+        PrestoTopicFieldVariableResolver variableResolver = new PrestoTopicFieldVariableResolver(getSchemaProvider());
+        String name = PatternValueUtils.getValueByPattern(variableResolver, topic, getConfig());
+        if (name == null) {
+            name = topic.getName(field);
         }
         return name;
-    }
-
-    private String replacePattern(Pattern p, String pattern, String name, PrestoTopic topic) {
-        String result = pattern;
-        Matcher matcher = p.matcher(pattern);
-        while (matcher.find()) {
-            String variable = matcher.group(1);
-            String replacement = getValue(topic, variable);
-            result = result.replaceFirst("\\$\\{" + variable + "\\}", replacement);
-        }
-        return result;
-    }
-
-    private String getValue(PrestoTopic topic, String variable) {
-        PrestoSchemaProvider schemaProvider = getSchemaProvider();
-        PrestoTopicFieldVariableResolver variableResolver = new PrestoTopicFieldVariableResolver(schemaProvider);
-        List<String> values = variableResolver.getValues(topic, variable);
-        StringBuilder sb = new StringBuilder();
-        for (int i=0; i < values.size(); i++) {
-            if (i > 0) {
-                sb.append(", ");
-            }
-            sb.append(values.get(i));
-        }
-        return sb.toString();
     }
 
 }
---
+++ presto-jaxrs/src/main/java/net/ontopia/presto/jaxrs/process/impl/PatternValueUtils.java
@@ -0,0 +1,48 @@
+package net.ontopia.presto.jaxrs.process.impl;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import net.ontopia.presto.spi.utils.PrestoVariableResolver;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.node.ObjectNode;
+
+public class PatternValueUtils {
+
+    public static String getValueByPattern(PrestoVariableResolver variableResolver, Object value, ObjectNode config) {
+        if (config != null) {
+            JsonNode patternNode = config.path("pattern");
+            if (patternNode.isTextual()) {
+                String pattern = patternNode.getTextValue();
+                return getValueByPattern(variableResolver, value, pattern);
+            }
+        }
+        return null;
+    }
+
+    public static String getValueByPattern(PrestoVariableResolver variableResolver, Object value, String pattern) {
+        String result = pattern;
+        Matcher matcher = Pattern.compile("\\$\\{(\\w+)\\}").matcher(pattern);
+        while (matcher.find()) {
+            String variable = matcher.group(1);
+            String replacement = getValue(variableResolver, value, variable);
+            result = result.replaceFirst("\\$\\{" + variable + "\\}", replacement);
+        }
+        return result;
+    }
+
+    private static String getValue(PrestoVariableResolver variableResolver, Object value, String variable) {
+        List<String> values = variableResolver.getValues(value, variable);
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i < values.size(); i++) {
+            if (i > 0) {
+                sb.append(", ");
+            }
+            sb.append(values.get(i));
+        }
+        return sb.toString();
+    }
+
+}
