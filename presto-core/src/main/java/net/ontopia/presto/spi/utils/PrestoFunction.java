91afb240d9371b8f23f27d60825da0e5fae18ea2
Geir Ove Grønmo <grove@geirove.org>
2013-12-17 08:11:07 +0100

    Added config parameter to PrestoFunction.

--- presto-core/src/main/java/net/ontopia/presto/spi/utils/PrestoFunction.java
+++ presto-core/src/main/java/net/ontopia/presto/spi/utils/PrestoFunction.java
@@ -3,12 +3,14 @@ package net.ontopia.presto.spi.utils;
 import java.util.Collection;
 import java.util.List;
 
+import org.codehaus.jackson.node.ObjectNode;
+
 import net.ontopia.presto.spi.PrestoField;
 import net.ontopia.presto.spi.PrestoTopic.Paging;
 
 public interface PrestoFunction {
 
-    List<Object> execute(PrestoVariableContext context, Collection<? extends Object> objects, 
+    List<Object> execute(PrestoVariableContext context, ObjectNode config, Collection<? extends Object> objects, 
             PrestoField field, Paging paging);
 
 }
