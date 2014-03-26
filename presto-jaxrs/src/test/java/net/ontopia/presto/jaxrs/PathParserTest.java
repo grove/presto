b79eb272fd4be383562f7fcf05ce192d4f4f39cd
Geir Ove Grønmo <grove@geirove.org>
2014-03-26 08:02:03 +0100

    More complete PathParser tests.

--- presto-jaxrs/src/test/java/net/ontopia/presto/jaxrs/PathParserTest.java
+++ presto-jaxrs/src/test/java/net/ontopia/presto/jaxrs/PathParserTest.java
@@ -1,6 +1,9 @@
 package net.ontopia.presto.jaxrs;
 
+import net.ontopia.presto.spi.PrestoField;
+import net.ontopia.presto.spi.PrestoTopic;
 import net.ontopia.presto.spi.utils.PrestoContext;
+import net.ontopia.presto.spi.utils.PrestoContextField;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -21,7 +24,14 @@ public class PathParserTest extends AbstractPrestoTest  {
 
     @Test
     public void testGetContextField() {
-
+        PrestoContextField contextField = PathParser.getContextField(session, "o:root$pinfo$children$o:child1$cinfo$grandchildren");
+        
+        PrestoContext context = contextField.getContext();
+        PrestoTopic topic = context.getTopic();
+        Assert.assertEquals(topic.getId(), "o:child1");
+        
+        PrestoField field = contextField.getField();
+        Assert.assertEquals(field.getId(), "grandchildren");
     }
 
     @Test
@@ -37,9 +47,31 @@ public class PathParserTest extends AbstractPrestoTest  {
     }
 
     @Test
+    public void testGetTopicByPathChildDirect() {
+        PrestoContext topic = PathParser.getTopicByPath(session, "_", "o:child1", "cinfo");
+        Assert.assertEquals(topic.getTopicId(), "o:child1");
+    }
+
+    @Test
     public void testGetTopicByPathGrandchild() {
         PrestoContext topic = PathParser.getTopicByPath(session, "o:root$pinfo$children$o:child1$cinfo$grandchildren", "o:grandchild1", "ginfo");
         Assert.assertEquals(topic.getTopicId(), "o:grandchild1");
     }
 
+    @Test
+    public void testGetTopicByPathGrandchildViaChild() {
+        PrestoContext topic = PathParser.getTopicByPath(session, "o:child1$cinfo$grandchildren", "o:grandchild1", "ginfo");
+        Assert.assertEquals(topic.getTopicId(), "o:grandchild1");
+    }
+
+    @Test
+    public void testGetInlineTopicPath() {
+        String inPath = "o:root$pinfo$children$o:child1$cinfo$grandchildren";
+        PrestoContextField contextField = PathParser.getContextField(session, inPath);
+        PrestoContext context = contextField.getContext();
+        PrestoField field = contextField.getField();
+        String outPath = PathParser.getInlineTopicPath(context, field);
+        Assert.assertEquals(outPath, inPath);
+    }
+    
 }
